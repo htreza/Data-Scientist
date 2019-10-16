9c6d6a4c537a8d93c63f6b51a9a215b40cbf3cb9
Henrique Treza <trezah.it@outlook.com>
2019-10-15 23:25:07 -0300

    Create amostragem_sistematica.py

---
+++ Exemplos de Amostragem/amostragem_sistematica.py
@@ -0,0 +1,23 @@
+"""
+@author: Henrique Treza
+"""
+
+import pandas as pd
+import numpy as np 
+from math import ceil
+
+populacao = 150
+amostra = 15
+k = ceil(populacao / amostra)
+
+r = np.random.randint(low = 1, high = k + 1, size = 1)
+
+acumulador = r[0]
+sorteados = []
+for i in range(amostra):
+    #print(acumulador)
+    sorteados.append(acumulador)
+    acumulador += k
+    
+base = pd.read_csv('iris.csv')
+base_final = base.loc[sorteados]    
