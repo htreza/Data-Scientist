38f0d272f30e93a1a157cdb2089e1338f00bc40b
Henrique Treza <trezah.it@outlook.com>
2020-04-23 17:22:04 -0300

    Create desafio04.py

---
+++ Desafios_Quarentena/desafio04.py
@@ -0,0 +1,10 @@
+# Developed by Henrique Treza
+
+#Desafio 4
+
+
+#Arredondar as médias (coluna de nota média) para duas casas decimais.
+
+
+filmes_com_media_e_votos['nota_media'] = filmes_com_media_e_votos['nota_media'].round(2)
+filmes_com_media_e_votos 
