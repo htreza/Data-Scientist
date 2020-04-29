9b1a7bcd6b125c38c34221ee557a2a4af703ef53
Henrique Treza <trezah.it@outlook.com>
2020-04-29 12:40:04 -0300

    Create desafio09.py

---
+++ Desafios_Quarentena/desafio09.py
@@ -0,0 +1,14 @@
+# Developed by Henrique Treza
+
+#Desafio 9
+
+
+#Encontar vários filmes com médias próximas e distribuições diferentes, use a função plot_filmes(n) para plotar.
+
+
+plot_filme(924) # 2001: A Space Odyssey (1968)
+
+plot_filme(55820) # No Country for Old Men (2007)
+
+plot_filme(2150) # Gods Must Be Crazy, The (1980)
+
