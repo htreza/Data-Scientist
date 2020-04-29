5bd37e8bc43adf35ce955bbf87530cd86b1cd59d
Henrique Treza <trezah.it@outlook.com>
2020-04-29 12:40:25 -0300

    Create desafio15.py

---
+++ Desafios_Quarentena/desafio15.py
@@ -0,0 +1,8 @@
+# Developed by Henrique Treza
+
+#Desafio 15
+
+
+#Plotar e analisar o Boxplot da média (coluna imbd_score) dos filmes em preto e branco e coloridos.
+
+sns.boxplot(x='color', y='imdb_score', data=color_or_bw)
